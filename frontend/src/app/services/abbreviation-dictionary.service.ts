022e3449e6eb3b54a0fcf16534c4966f8087be73
Arif Rezza <65624121+arifrezza@users.noreply.github.com>
2026-03-11 12:03:54 +0530

    Tone correction phase1

--- frontend/src/app/services/abbreviation-dictionary.service.ts
+++ frontend/src/app/services/abbreviation-dictionary.service.ts
@@ -12,6 +12,7 @@ export class AbbreviationDictionaryService {
 
   private readonly dictionary = new Map<string, string>([
     ['u', 'you'],
+    ['uat', 'uat'],
     ['usr', 'user'],
     ['bcoz', 'because'],
     ['ur', 'your'],
